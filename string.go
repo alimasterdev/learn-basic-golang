6aa1e9b3cdb9344b02d8a807fa7f3a9438bcb736
Muhammad Ali Imron <ali.master.dev@gmail.com>
2022-02-06 13:55:04 +0700

    First commit string.go

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Ali")
+	fmt.Println("Imron")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Ali"[0])
+	fmt.Println("Imron"[1])
+}
